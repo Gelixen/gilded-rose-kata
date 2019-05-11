6b5266d2573e260a6ce5ee84a66213e450e2c4b8
Karolis Šlipaitis <slipaitis.karolis+github@gmail.com>
2019-05-11 19:17:13 +0300

    Change used item list in texttestFixture from starting one to the app itself, because starting one is not being modified

--- src/test/java/com/gildedrose/TexttestFixture.java
+++ src/test/java/com/gildedrose/TexttestFixture.java
@@ -26,7 +26,7 @@ public class TexttestFixture {
         for (int i = 0; i < days; i++) {
             System.out.println("-------- day " + i + " --------");
             System.out.println("name, sellIn, quality");
-            for (Item item : items) {
+            for (Item item : app.getItems()) {
                 System.out.println(item);
             }
             System.out.println();
