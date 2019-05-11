4d668c7fa4af8954565c121de0838cc467cf5668
Karolis Šlipaitis <slipaitis.karolis+github@gmail.com>
2019-05-11 15:41:34 +0300

    Cover all if cases with tests

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -12,6 +12,11 @@ import java.util.Collection;
 @RunWith(Parameterized.class)
 public class GildedRoseTest {
 
+    private static final String RANDOM_NAME = "foo";
+    private static final String AGED_BRIE = "Aged Brie";
+    private static final String TAFKAL = "Backstage passes to a TAFKAL80ETC concert";
+    private static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
+
     private Item startingItem;
     private Item expectedItem;
 
@@ -23,16 +28,37 @@ public class GildedRoseTest {
     @Parameterized.Parameters(name = "{0} -> {1}")
     public static Collection primeNumbers() {
         return Arrays.asList(new Object[][] {
-                { new Item("foo", 1, 0), new Item("foo", 0, 0) },
-                { new Item("foo", 1, 15), new Item("foo", 0, 14) },
-                { new Item("foo", 1, 50), new Item("foo", 0, 49) },
-                { new Item("foo", -5, 5), new Item("foo", -6, 3) },
-                { new Item("foo", 0, 5), new Item("foo", -1, 3) },
-                { new Item("foo", 3, 5), new Item("foo", 2, 4) },
-                { new Item("foo", 6, 5), new Item("foo", 5, 4) },
-                { new Item("foo", 8, 5), new Item("foo", 7, 4) },
-                { new Item("foo", 11, 5), new Item("foo", 10, 4) },
-                { new Item("foo", 15, 5), new Item("foo", 14, 4) }
+                { new Item(RANDOM_NAME, 1, 0), new Item(RANDOM_NAME, 0, 0) },
+                { new Item(RANDOM_NAME, 1, 15), new Item(RANDOM_NAME, 0, 14) },
+                { new Item(RANDOM_NAME, 1, 50), new Item(RANDOM_NAME, 0, 49) },
+
+                { new Item(RANDOM_NAME, -5, 5), new Item(RANDOM_NAME, -6, 3) },
+                { new Item(RANDOM_NAME, 0, 5), new Item(RANDOM_NAME, -1, 3) },
+                { new Item(RANDOM_NAME, 3, 5), new Item(RANDOM_NAME, 2, 4) },
+                { new Item(RANDOM_NAME, 6, 5), new Item(RANDOM_NAME, 5, 4) },
+                { new Item(RANDOM_NAME, 8, 5), new Item(RANDOM_NAME, 7, 4) },
+                { new Item(RANDOM_NAME, 11, 5), new Item(RANDOM_NAME, 10, 4) },
+                { new Item(RANDOM_NAME, 15, 5), new Item(RANDOM_NAME, 14, 4) },
+
+                { new Item(AGED_BRIE, 15, 60), new Item(AGED_BRIE, 14, 60) }, // Q >= 50 & S >= 0    Q (no change)
+                { new Item(AGED_BRIE, -5, 60), new Item(AGED_BRIE, -6, 60) }, // Q >= 50 & S < 0    Q (no change)
+                { new Item(AGED_BRIE, 15, 10), new Item(AGED_BRIE, 14, 11) }, // Q < 50 & S >= 0    Q + 1
+                { new Item(AGED_BRIE, -5, 49), new Item(AGED_BRIE, -6, 50) }, // Q < 50 & S < 0    Q + 1
+                { new Item(AGED_BRIE, -5, 10), new Item(AGED_BRIE, -6, 12) }, // Q < 49 & S < 0    Q + 2
+
+                { new Item(TAFKAL, 15, 60), new Item(TAFKAL, 14, 60) }, // Q >= 50    Q (no change)
+                { new Item(TAFKAL, 15, 40), new Item(TAFKAL, 14, 41) }, // Q < 50 & S >= 11    Q + 1
+                { new Item(TAFKAL, 10, 49), new Item(TAFKAL, 9, 50) }, // Q < 50 & 11 < S <= 6    Q + 1
+                { new Item(TAFKAL, 10, 45), new Item(TAFKAL, 9, 47) }, // Q < 49 & 11 < S <= 6    Q + 2
+                { new Item(TAFKAL, 5, 48), new Item(TAFKAL, 4, 50) }, // Q < 49 & S < 6    Q + 2
+                { new Item(TAFKAL, 5, 46), new Item(TAFKAL, 4, 49) }, // Q < 48 & S < 6    Q + 3
+
+                { new Item(SULFURAS, 15, 60), new Item(SULFURAS, 15, 60) }, // Q >= 50 & S >= 0    Q & S no changes
+                { new Item(SULFURAS, 15, 60), new Item(SULFURAS, 15, 60) }, // Q >= 50 & S < 0    Q & S no changes
+                { new Item(SULFURAS, 15, 5), new Item(SULFURAS, 15, 5) }, // 0 < Q < 50 & S >= 0    Q & S no changes
+                { new Item(SULFURAS, 15, 5), new Item(SULFURAS, 15, 5) }, // 0 < Q < 50 & S < 0    Q & S no changes
+                { new Item(SULFURAS, 15, 0), new Item(SULFURAS, 15, 0) }, // Q = 0 & S >= 0    Q & S no changes
+                { new Item(SULFURAS, 15, 0), new Item(SULFURAS, 15, 0) }, // Q = 0 & S < 0    Q & S no changes
         });
     }
 
