28510cdc9f8df1d4e6976789eacf931436b2690f
Karolis Šlipaitis <slipaitis.karolis@gmail.com>
2019-05-11 12:28:50 +0300

    Initial test refactored to pass and additionally test how `quality`, `sellIn` values updated.

--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -7,11 +7,30 @@ import org.junit.Test;
 public class GildedRoseTest {
 
     @Test
-    public void foo() {
-        Item[] items = new Item[] { new Item("foo", 0, 0) };
+    public void zeroSellInAndQualityItem_updateOnce_minusOneSellInZeroQuality() {
+        Item[] items = createItems(1);
+
         GildedRose app = new GildedRose(items);
         app.updateQuality();
-        assertEquals("fixme", app.items[0].name);
+
+        assertEquals(1, app.items.length);
+        assertEquals("foo", app.items[0].name);
+        assertEquals(-1, app.items[0].sellIn);
+        assertEquals(0, app.items[0].quality);
+    }
+
+    private Item[] createItems(int count) {
+        Item[] items = new Item[count];
+
+        for (int i = 0; i < count; i++) {
+            items[i] = createItem();
+        }
+
+        return items;
+    }
+
+    private Item createItem() {
+        return new Item("foo", 0, 0);
     }
 
 }
