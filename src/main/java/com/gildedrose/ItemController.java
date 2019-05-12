dea70b0799d6223f2c06adb6ecab51291993c91c
Karolis Šlipaitis <slipaitis.karolis+github@gmail.com>
2019-05-12 15:37:10 +0300

    Add mongoDB and it's properties. Replace controller to return items from db instead of the hardcoded items

--- src/main/java/com/gildedrose/ItemController.java
+++ src/main/java/com/gildedrose/ItemController.java
@@ -3,14 +3,19 @@ package com.gildedrose;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class ItemController {
 
+    private final ItemService itemService;
+
+    public ItemController(ItemService itemService) {
+        this.itemService = itemService;
+    }
+
     @RequestMapping("/")
-    public Item[] index() {
-        Item item = new Item("foo", 1, 1);
-        Item item2 = new Item("foo2", 2, 2);
-        Item item3 = new Item("foo3", 3, 3);
-        return new Item[] { item, item2, item3 };
+    public List<Item> index() {
+        return itemService.findAll();
     }
 }
---
+++ src/main/java/com/gildedrose/ItemService.java
@@ -0,0 +1,19 @@
+package com.gildedrose;
+
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    public ItemService(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+    public List<Item> findAll() {
+        return itemRepository.findAll();
+    }
+}
