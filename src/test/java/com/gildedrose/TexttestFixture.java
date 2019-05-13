20109743a9675e9f17556ea960acae553053f11d
Karolis Šlipaitis <slipaitis.karolis+github@gmail.com>
2019-05-13 11:18:12 +0300

    Removed unnecessary method throw

--- src/test/java/com/gildedrose/TexttestFixture.java
+++ src/test/java/com/gildedrose/TexttestFixture.java
@@ -1,7 +1,7 @@
 package com.gildedrose;
 
 public class TexttestFixture {
-    public static void main(String[] args) throws InterruptedException {
+    public static void main(String[] args) {
         System.out.println("OMGHAI!");
 
         Item[] items = new Item[] {
