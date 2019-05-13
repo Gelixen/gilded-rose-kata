decad00f62b88f7d1a510fc523c59d7fc7d0aefb
Karolis Šlipaitis <slipaitis.karolis+github@gmail.com>
2019-05-13 13:19:49 +0300

    Remove item name constants from GildedRose class and in tests use ItemType enum names for item names and rename some constants

--- src/main/java/com/gildedrose/core/GildedRose.java
+++ src/main/java/com/gildedrose/core/GildedRose.java
@@ -5,10 +5,6 @@ import java.util.concurrent.CompletableFuture;
 
 public class GildedRose {
 
-    public static final String AGED_BRIE = "Aged Brie";
-    public static final String TAFKAL = "Backstage passes to a TAFKAL80ETC concert";
-    public static final String SULFURAS = "Sulfuras, Hand of Ragnaros";
-
     Item[] items;
 
     public GildedRose(Item[] items) {
--- src/test/java/com/gildedrose/core/GildedRoseTest.java
+++ src/test/java/com/gildedrose/core/GildedRoseTest.java
@@ -7,14 +7,16 @@ import org.junit.runners.Parameterized;
 import java.util.Arrays;
 import java.util.Collection;
 
-import static com.gildedrose.core.GildedRose.*;
 import static org.junit.Assert.assertEquals;
 
 @RunWith(Parameterized.class)
 public class GildedRoseTest {
 
-    private static final String RANDOM_NAME = "foo";
-    private static final String RANDOM_CONJURED_NAME = "Conjured foo";
+    private static final String RANDOM_BASIC_ITEM = "foo";
+    private static final String AGED_BRIE = ItemType.AGED_BRIE.getName();
+    private static final String TAFKAL = ItemType.TAFKAL.getName();
+    private static final String SULFURAS = ItemType.SULFURAS.getName();
+    private static final String CONJURED_RANDOM_ITEM = ItemType.CONJURED.getName() + " " + RANDOM_BASIC_ITEM;
 
     private Item startingItem;
     private Item expectedItem;
@@ -27,17 +29,17 @@ public class GildedRoseTest {
     @Parameterized.Parameters(name = "{0} -> {1}")
     public static Collection primeNumbers() {
         return Arrays.asList(new Object[][] {
-                { new Item(RANDOM_NAME, 1, 0), new Item(RANDOM_NAME, 0, 0) },
-                { new Item(RANDOM_NAME, 1, 15), new Item(RANDOM_NAME, 0, 14) },
-                { new Item(RANDOM_NAME, 1, 50), new Item(RANDOM_NAME, 0, 49) },
+                { new Item(RANDOM_BASIC_ITEM, 1, 0), new Item(RANDOM_BASIC_ITEM, 0, 0) },
+                { new Item(RANDOM_BASIC_ITEM, 1, 15), new Item(RANDOM_BASIC_ITEM, 0, 14) },
+                { new Item(RANDOM_BASIC_ITEM, 1, 50), new Item(RANDOM_BASIC_ITEM, 0, 49) },
 
-                { new Item(RANDOM_NAME, -5, 5), new Item(RANDOM_NAME, -6, 3) }, // S < 0    Q - 2
-                { new Item(RANDOM_NAME, 0, 5), new Item(RANDOM_NAME, -1, 3) }, // S < 0    Q - 2
-                { new Item(RANDOM_NAME, 3, 5), new Item(RANDOM_NAME, 2, 4) }, // S> 0    Q - 1
-                { new Item(RANDOM_NAME, 6, 5), new Item(RANDOM_NAME, 5, 4) }, // S> 0    Q - 1
-                { new Item(RANDOM_NAME, 8, 5), new Item(RANDOM_NAME, 7, 4) }, // S> 0    Q - 1
-                { new Item(RANDOM_NAME, 11, 5), new Item(RANDOM_NAME, 10, 4) }, // S> 0    Q - 1
-                { new Item(RANDOM_NAME, 15, 5), new Item(RANDOM_NAME, 14, 4) }, // S> 0    Q - 1
+                { new Item(RANDOM_BASIC_ITEM, -5, 5), new Item(RANDOM_BASIC_ITEM, -6, 3) }, // S < 0    Q - 2
+                { new Item(RANDOM_BASIC_ITEM, 0, 5), new Item(RANDOM_BASIC_ITEM, -1, 3) }, // S < 0    Q - 2
+                { new Item(RANDOM_BASIC_ITEM, 3, 5), new Item(RANDOM_BASIC_ITEM, 2, 4) }, // S> 0    Q - 1
+                { new Item(RANDOM_BASIC_ITEM, 6, 5), new Item(RANDOM_BASIC_ITEM, 5, 4) }, // S> 0    Q - 1
+                { new Item(RANDOM_BASIC_ITEM, 8, 5), new Item(RANDOM_BASIC_ITEM, 7, 4) }, // S> 0    Q - 1
+                { new Item(RANDOM_BASIC_ITEM, 11, 5), new Item(RANDOM_BASIC_ITEM, 10, 4) }, // S> 0    Q - 1
+                { new Item(RANDOM_BASIC_ITEM, 15, 5), new Item(RANDOM_BASIC_ITEM, 14, 4) }, // S> 0    Q - 1
 
                 { new Item(AGED_BRIE, 15, 50), new Item(AGED_BRIE, 14, 50) }, // Q = 50 & S >= 0    Q (no change)
                 { new Item(AGED_BRIE, -5, 50), new Item(AGED_BRIE, -6, 50) }, // Q = 50 & S < 0    Q (no change)
@@ -60,10 +62,10 @@ public class GildedRoseTest {
                 { new Item(SULFURAS, 15, 0), new Item(SULFURAS, 15, 0) }, // Q = 0 & S >= 0    Q & S no changes
                 { new Item(SULFURAS, 15, 0), new Item(SULFURAS, 15, 0) }, // Q = 0 & S < 0    Q & S no changes
 
-                { new Item(RANDOM_CONJURED_NAME, 4, 15), new Item(RANDOM_CONJURED_NAME, 3, 13) }, // S > 0    Q - 2
-                { new Item(RANDOM_CONJURED_NAME, 4, 1), new Item(RANDOM_CONJURED_NAME, 3, 0) }, // Q = 1 & S > 0    Q - 1
-                { new Item(RANDOM_CONJURED_NAME, 0, 15), new Item(RANDOM_CONJURED_NAME, -1, 11) }, // S <= 0    Q - 4
-                { new Item(RANDOM_CONJURED_NAME, 0, 3), new Item(RANDOM_CONJURED_NAME, -1, 0) }, // Q = 3 & S <= 0    Q - 1
+                { new Item(CONJURED_RANDOM_ITEM, 4, 15), new Item(CONJURED_RANDOM_ITEM, 3, 13) }, // S > 0    Q - 2
+                { new Item(CONJURED_RANDOM_ITEM, 4, 1), new Item(CONJURED_RANDOM_ITEM, 3, 0) }, // Q = 1 & S > 0    Q - 1
+                { new Item(CONJURED_RANDOM_ITEM, 0, 15), new Item(CONJURED_RANDOM_ITEM, -1, 11) }, // S <= 0    Q - 4
+                { new Item(CONJURED_RANDOM_ITEM, 0, 3), new Item(CONJURED_RANDOM_ITEM, -1, 0) }, // Q = 3 & S <= 0    Q - 1
         });
     }
 
